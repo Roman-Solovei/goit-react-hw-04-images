bf0f15479fdb5ec33ec497b388312e1d64533b9f
Roman-Solovei <romario2409@gmail.com>
2022-04-08 23:19:43 +0300

    images hook

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,55 +1,78 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropType from "prop-types"
 import s from "./Modal.module.css"
 
-
-export default class Modal extends Component { 
-    // state = {
-    //     visible: false,
-    //     modalImg: ""
-    // };
+export default function Modal({ modalToggle, image, tags }) { 
     
-    // toggle = (image) => {
-    // this.setState(prevState => ({
-    //     visible: !prevState.visible,
-    //     modalImg:image
-    // }));
-    // };
-    
-    componentDidMount(){
-    window.addEventListener('keydown', this.onEscPress)
-    }
+    useEffect(() => {
+        window.addEventListener("keydown", onEscPress);
 
-    onEscPress=(event)=>{
+        return () => {
+        window.removeEventListener("keydown", onEscPress);
+        };
+    });   
+    
+    const onEscPress = (event) => {
         if (event.code === 'Escape') {
-            this.props.modalToggle()
+            modalToggle()
         }
     }
-
-    componentWillUnmount(){
-        window.removeEventListener('keydown', this.onEscPress)
-    }
-
-    render() { 
-        // const { visible } = this.state;
-        console.log(this.props)
-        return (
-            
-            <div className={s.Overlay} onClick={(event) => {
-                console.log(event)
-                if (event.target === event.currentTarget) {
-                    this.props.modalToggle()
-                }
-            }}>
-                <div className={ s.Modal }>
-                    <img src={ this.props.image } alt={ this.props.tags } />
-                </div>
+    
+    return (
+                
+        <div className={s.Overlay} onClick={(event) => {
+            // console.log(event)
+            if (event.target === event.currentTarget) {
+                modalToggle()
+            }
+        }}>
+            <div className={ s.Modal }>
+                <img src={ image } alt={ tags } />
             </div>
-        )
-    }
+        </div>
+    )    
 };
 
 Modal.protoTypes = {
     modalToggle: PropType.func.isRequired,
     image:PropType.string.isRequired
 }
+
+// export default class Modal extends Component { 
+   
+//     componentDidMount(){
+//     window.addEventListener('keydown', this.onEscPress)
+//     }
+
+//     onEscPress=(event)=>{
+//         if (event.code === 'Escape') {
+//             this.props.modalToggle()
+//         }
+//     }
+
+//     componentWillUnmount(){
+//         window.removeEventListener('keydown', this.onEscPress)
+//     }
+
+//     render() {     
+//         // console.log(this.props)
+//         return (
+            
+//             <div className={s.Overlay} onClick={(event) => {
+//                 console.log(event)
+//                 if (event.target === event.currentTarget) {
+//                     this.props.modalToggle()
+//                 }
+//             }}>
+//                 <div className={ s.Modal }>
+//                     <img src={ this.props.image } alt={ this.props.tags } />
+//                 </div>
+//             </div>
+//         )
+//     }
+// };
+
+// Modal.protoTypes = {
+//     modalToggle: PropType.func.isRequired,
+//     image:PropType.string.isRequired
+// }
